4ff582f98e89656e75724526f1ed34bbac03d471
OFFtiex <gera.maslov.39@gmail.com>
2025-11-20 01:11:42 +0200

    add

---
+++ lab07/main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+
+using Vector = std::vector<int>;
+using Array = std::array<int, 10>;
+// Пункт 1 
+
+void sort1(Array arr) {
+	std::sort(arr.begin(), arr.end());
+	std::cout << "Addres in the function: " << &arr << std::endl;
+	for (int i = 0; i < arr.size(); i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+void sort2(Array& arr) {
+	std::sort(arr.begin(), arr.end());
+	std::cout << "Addres in the function: " << &arr << std::endl;
+	for (int i = 0; i < arr.size(); i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+void sort3(Array* arr) {
+	std::sort((*arr).begin(), (*arr).end());
+	std::cout << "Addres in the function: " << arr << std::endl;
+	std::cout << "Sorted array by pointer:" << std::endl;
+	for (int i = 0; i < (*arr).size(); i++) {
+		std::cout << (*arr)[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+
+
+
+/*
+void null() {
+	std::cout << "Finish" << std::endl;
+}
+void first(const Vector& vec) {
+	std::cout << "Vector: [ ";
+	for (int i = 0;  i < vec.size(); i++) {
+		std::cout << vec[i]  << " ";
+	}
+	std::cout << "]" << std::endl;
+}
+void second(Vector& vec) {
+	int x;
+	std::cout << "Enter the element of vector: " << std::endl;
+	std::cin >> x;
+	vec.insert(vec.begin(), x);
+}
+void third(Vector& vec) {
+	int x;
+	std::cout << "Enter the element of vector: " << std::endl;
+	std::cin >> x;
+	vec.push_back(x);
+}
+void fourth(Vector& vec) {
+	vec.clear();
+}
+void fifth(Vector& vec) {
+	int x;
+	std::cout << "Enter the index of the element of vector: " << std::endl;
+	std::cin >> x;
+	if (x >= vec.size()) {
+		std::cout << "Incorrect index" << std::endl;
+	}
+	else {
+		std::cout << vec[x] << std::endl;
+	}
+}
+void sixth(Vector& vec) {
+	first(vec);
+	if (vec.size() % 2 == 0) {
+		vec.erase(vec.begin());
+	}
+	else {
+		if (vec.size() >= 4) {
+			vec.erase(vec.end() - 4, vec.end());
+		}
+		else {
+			vec.erase(vec.end() - vec.size(), vec.end());
+		}
+	}
+	first(vec);
+}
+*/
+
+int main() {
+	/*
+	int num;
+	Vector vec;
+	vec.reserve(100'000);
+
+	do {
+		std::cout << "Menu" << std::endl;
+		std::cout << "0 - Exit" << std::endl;
+		std::cout << "1 - Show a vector" << std::endl;
+		std::cout << "2 - Add element in the start of the vector" << std::endl;
+		std::cout << "3 - Add element in the end of the vector" << std::endl;
+		std::cout << "4 - Clear the vector" << std::endl;
+		std::cout << "5 - Find the element" << std::endl;
+		std::cout << "6 - Delete elements" << std::endl;
+		std::cout << "Number: " << std::endl;
+		std::cin >> num;
+
+		switch (num) {
+		case 0: 
+			break;
+		case 1:
+			int i;
+			first(vec);
+			break;
+		case 2:
+			second(vec);
+			break;
+		case 3:
+			third(vec);
+			break;
+		case 4:
+			fourth(vec);
+			break;
+		case 5:
+			fifth(vec);
+			break;
+		case 6:
+			sixth(vec);
+			break;
+		default:
+			std::cout << "Inccorect command" << std::endl;
+		}
+	} while (num != 0);
+	
+	null();
+	*/
+
+	// Пункт 2
+
+	Array arr;
+
+	std::cout << "Enter array [-10,10]: " << std::endl;
+	for (int i = 0; i < arr.size(); i++) {
+		std::cin >> arr[i];
+	}
+	
+	sort1(arr);
+	sort2(arr);
+	sort3(&arr);
+
+
+	return 0;
+}
+
